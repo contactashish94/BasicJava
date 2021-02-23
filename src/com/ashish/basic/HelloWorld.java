872a8653368811843ba19a9e5b227ed1858bcd01
contactashish94 <contactashish94@gmail.com>
2021-02-23 11:07:25 +0530

    first commit

--- src/com/ashish/basic/HelloWorld.java
+++ src/com/ashish/basic/HelloWorld.java
@@ -3,5 +3,10 @@ package com.ashish.basic;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello-World!");
+
+        int i = 10;
+        int i1=10;
+
+        System.out.println(i+i1);
     }
 }
---
+++ src/com/ashish/basic/byteshortintlong/Main.java
@@ -0,0 +1,17 @@
+package com.ashish.basic.byteshortintlong;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value = 10000;
+
+        int min_range = Integer.MIN_VALUE;
+
+        int max_value = Integer.MAX_VALUE;
+
+        System.out.println(min_range);
+        System.out.println("------");
+
+        System.out.println(max_value);
+    }
+}
