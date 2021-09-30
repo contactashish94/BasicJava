ec7b4a4a05101c23b80129edf31d6eaf761516cf
contactashish94 <contactashish94@gmail.com>
2021-09-30 09:00:35 +0530

    added leetcode

---
+++ src/cimpress/Test2.java
@@ -0,0 +1,21 @@
+package cimpress;
+
+public class Test2 {
+    public static void main(String[] args) {
+        BasicLinkedList<String> list = new BasicLinkedList();
+        //ds.BasicLinkedList<String> list = new ds.BasicLinkedList();
+
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        list.add("6");
+        list.add("7");
+        list.add("8");
+        list.add("9");
+        list.add("10");
+        System.out.println(list.toString());
+        list.traverseByN(2);
+    }
+}
